10be0a52469373ffd109e8b8d538e99a596fcb1e
Antoine Lafontaine <antoine.lafontaine@cossette.com>
2016-04-29 09:57:47 -0400

    Refactoring of where we keep personal and projects settings

--- includes/config.sh
+++ includes/config.sh
@@ -2,28 +2,28 @@
 
 configuration.setup() {
 
-  if [ ! -f ~/.dagon/config ]; then
-    m.status "Creating ~/.dagon/config"
-    touch /Users/${username}/.dagon/config
-    /Users/${username}/Library/Python/2.7/bin/crudini --set ~/.dagon/config core
-    /Users/${username}/Library/Python/2.7/bin/crudini --set ~/.dagon/config project
+  if [ ! -f ~/.dagonconfig ]; then
+    m.status "Creating ~/.dagonconfig"
+    touch /Users/${username}/.dagonconfig
+    /Users/${username}/Library/Python/2.7/bin/crudini --set ~/.dagonconfig core
+    /Users/${username}/Library/Python/2.7/bin/crudini --set ~/.dagonconfig project
   else
-    m.status "Reading Dagon configuration from ~/.dagon/config"
+    m.status "Reading Dagon configuration from ~/.dagonconfig"
   fi
 
-  config_repo="$(/Users/${username}/Library/Python/2.7/bin/crudini --get ~/.dagon/config core config_repo)"
-  echo $config_repo | grep --quiet '^["git"|"ssh"|"http"|"https"|"git@"].*:.*[".git"|".git\/"]$'
+  settings_repo="$(crudini --get /Users/$username/.dagonconfig core settings_repo)"
   if [ $? == 0 ]; then
-    m.status "Using: $config_repo from: /usr/local/dagon/config"
+    m.status "Getting your personal and project settings from: ~/.dagon/settings (clone of $settings_repo)"
   else
-    m.status "\nSeems your config repository is not properly set."
-    m.status "Where is your config repository located?"
-    read -p "↪︎  " config_repo
+    m.status "\nSeems that your settings repository is not properly set."
+    m.status "(This is the repo containing your personal settings and projects)"
+    m.status "Where is your settings repository located?"
+    read -p "↪︎  " settings_repo
 
-    if [ -n "$config_repo" ]; then
-      echo $config_repo | grep --quiet '^["git"|"ssh"|"http"|"https"|"git@"].*:.*[".git"|".git\/"]$'
+    if [ -n "$settings_repo" ]; then
+      echo $settings_repo | grep --quiet '^["git"|"ssh"|"http"|"https"|"git@"].*:.*[".git"|".git\/"]$'
       if [ $? == 0 ]; then
-        /Users/${username}/Library/Python/2.7/bin/crudini --set ~/.dagon/config core config_repo ${config_repo}
+        /Users/${username}/Library/Python/2.7/bin/crudini --set /Users/${username}/.dagonconfig core settings_repo ${settings_repo}
       else
         m.error "This doesn't look like a git repo."
       fi
@@ -32,19 +32,19 @@ configuration.setup() {
     fi
   fi
 
-  project_base_path="$(/Users/${username}/Library/Python/2.7/bin/crudini --get ~/.dagon/config project project_base_path)"
+  project_base_path="$(crudini --get ~/.dagonconfig project project_base_path)"
   if [ -n "$project_base_path" ]; then
     m.status "Projects will be created in: $project_base_path"
   else
     m.status "\nSeems your base project path is not properly set."
-    m.status "Where do you want your projects to be located?"
+    m.status "Where do you want your projects to be located on your local machine?"
     read -p "↪︎  " project_base_path
 
     # expand path if tilde is used
     check_project_bash_path="${project_base_path/#\~/$HOME}"
 
     if [ -d "$check_project_bash_path" ]; then
-      /Users/${username}/Library/Python/2.7/bin/crudini --set ~/.dagon/config project project_base_path ${project_base_path}
+      /Users/${username}/Library/Python/2.7/bin/crudini --set ~/.dagonconfig project project_base_path ${project_base_path}
     else
       m.error "This folder doesn't seem to exist."
     fi
